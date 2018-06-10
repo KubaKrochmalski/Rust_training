2492cec58372dd4de6d41e4294351b75027f7bd9
Jakub Krochmalski <kuba.krochmalski@gmail.com>
2018-06-10 23:48:28 +0200

    enums continued

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -8,13 +8,33 @@ struct IPAdd {
     address: String,
 }
 
+// Putting data directly into each enum's variant
+enum IPAddress {
+    V4(u8, u8, u8, u8),
+    V6(String),
+} 
+
 fn main() {
-    // Creatin instance of an enum
+    // Creating instance of an enum
     let _version = IPAddV::V4;
     let _version = IPAddV::V6;
 
+    // Enum type variable inside a struct
     let home = IPAdd {
         version: IPAddV::V4,
         address: String::from("127.0.0.1"),
     };
+
+    // Enums with associated data
+    let home = IPAddress::V4(127, 0, 0, 1);
+    let work = IPAddress::V6(String::from("2001:db8::1428:57ab"));
+
+    // Printing complex enums
+    if let IPAddress::V4(n1, n2, n3, n4) = home {
+        println!("Home's IP: {}.{}.{}.{}", n1, n2, n3, n4);
+    }
+
+    if let IPAddress::V6(address) = work {
+        println!("Work's IP: {}", address);
+    }
 }
